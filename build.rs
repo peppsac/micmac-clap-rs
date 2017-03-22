4920df8f22d5d327e5cd0669c00fec8a78f29577
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2017-03-22 22:16:16 +0100

    Update script

--- build.rs
+++ build.rs
@@ -1,11 +1,14 @@
 #[macro_use]
 extern crate clap;
 
-use clap::{App, Arg, SubCommand, Shell};
+use clap::{App, Arg, SubCommand, Shell, AppSettings};
 
 fn main() {
     let yaml = load_yaml!("src/mm3d-cli.yml");
-    let mut app = App::from_yaml(yaml);
+    let mut app = App::from_yaml(yaml)
+    	.global_setting(AppSettings::HidePossibleValuesInHelp)
+        .setting(AppSettings::VersionlessSubcommands)
+    	.global_setting(AppSettings::DisableVersion);
 
     app.gen_completions("mm3d",          // We need to specify the bin name manually
                         Shell::Bash,      // Then say which shell to build completions for
